907cf5133168cbe52f4bfb79dd75d26302e52215
Ernesto <83133501+edonado2@users.noreply.github.com>
2022-10-30 12:58:57 -0400

    initial

---
+++ src/components/Others/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import "./styles.css";
+
+const Footer = () => {
+  return (
+    <footer className="mt-5">
+      <div className="d-flex justify-content-center align-items-center">
+        <p className="mr-1 credits-text">Desarrollado Por</p>
+        <a className="mhq" target="_blank" href="#">
+          Los Avengers
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
